d478d98a22016d7a4fc20121713fe53662f861a8
Karim Elsheikh <k_elsheikh18@cs.uni-kl.de>
2020-02-21 11:11:53 +0100

    Added run.bat, this file should be ran on Windows only, and it runs GAP and loads the file 1.g from this directory %HOMEDRIVE%%HOMEPATH%\eclipse-workspace\kWL\ This file basically runs a method that waits for "commands" from the compareColours() and compareTwo() methods present in the class 'kWLClass', the commands get written to a file named "Stream.txt" in the directory of the project (or the the working directory I guess?). Then it outputs the group number requested as a Permutation group (gets converted) in the file "Stream2.txt". Please note that it is initialized to a certain group order and gets incremented after all groups of that order are used. If you want to start running generateinvariants starting from some other order than 256 you should modify the file "1.g" to be initialized with that other order as well as the number of groups of that order (you can get them using 'NumberSmallGroups(order)'). Hopefully a more user-friendly version of this project will be uploaded in the future.

---
+++ run.bat
@@ -0,0 +1,37 @@
+@echo off
+
+setlocal DisableDelayedExpansion enableextensions
+
+set KeyName=HKEY_CURRENT_USER\Software\GAP
+for /F "usebackq skip=2 tokens=3" %%A IN (`REG QUERY %KeyName% /ve 2^>nul`) DO (
+    set InstallDirectory=%%A
+)
+
+copy %InstallDirectory%\bin\gap.bat %cd%\gap29548.tmp /Y >nul
+echo. 2>"%cd%\gap29548.tmp.bat"
+for /F "tokens=*" %%a in (gap29548.tmp) do (
+	set "myVar=%%a"
+	
+	setlocal enabledelayedexpansion
+	set test=!myVar:~0,8!
+	Rem // This line removes all Double Quotes from the String Test
+	Set test=!test:"=!
+	if "!test!"=="set HOME" (
+		>>"%cd%\gap29548.tmp.bat" (
+			echo set HOME=%HOMEDRIVE%%HOMEPATH%\eclipse-workspace\kWL
+		)
+	) else if "!test!"=="start G" (
+		>>"%cd%\gap29548.tmp.bat" (
+			echo !myVar! "%%HOME%%\1.g"
+		)
+	) else (
+		>>"%cd%\gap29548.tmp.bat" (
+			echo !myVar!
+		)
+	)
+	endlocal
+)
+goto :writtenTheBatchFileNowRunIt REM (to run GAP with the correct working Directory basically speaking)
+
+:writtenTheBatchFileNowRunIt
+call gap29548.tmp.bat
